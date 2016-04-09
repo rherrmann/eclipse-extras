9649ed3179e8f7004d6e50615d983a11334fddaa
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2016-04-09 17:55:26 +0200

    Extract image path to constant

--- com.codeaffine.extras.launch.test/src/com/codeaffine/extras/launch/internal/StartLaunchConfigMennuPDETest.java
+++ com.codeaffine.extras.launch.test/src/com/codeaffine/extras/launch/internal/StartLaunchConfigMennuPDETest.java
@@ -8,6 +8,7 @@ import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.menu.MHandledMenuItem;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenuContribution;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,13 +36,14 @@ public class StartLaunchConfigMennuPDETest {
       .map( MHandledMenuItem.class::cast )
       .orElse( null );
 
-    assertThat( element.getIconURI() ).endsWith( "/icons/etool16/start-launch-configuration.gif" );
+    assertThat( element.getIconURI() ).endsWith( Images.START_LAUNCH_CONFIGURATION );
     assertThat( element.getLabel() ).isNotEmpty();
     assertThat( element.getCommand().getElementId() ).isEqualTo( OpenLaunchDialogHander.COMMAND_ID );
   }
 
   @SuppressWarnings("cast")
   private static IEclipseContext getEclipseContext() {
-    return ( IEclipseContext )PlatformUI.getWorkbench().getActiveWorkbenchWindow().getService( IEclipseContext.class );
+    IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+    return ( IEclipseContext )activeWorkbenchWindow.getService( IEclipseContext.class );
   }
 }
