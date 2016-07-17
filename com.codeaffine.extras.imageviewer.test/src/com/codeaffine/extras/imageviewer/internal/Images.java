c45c9828876cfa071716d8ef71bb332e0c6161b1
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2016-07-17 19:29:44 +0200

    Remove unused test images

--- com.codeaffine.extras.imageviewer.test/src/com/codeaffine/extras/imageviewer/internal/Images.java
+++ com.codeaffine.extras.imageviewer.test/src/com/codeaffine/extras/imageviewer/internal/Images.java
@@ -5,9 +5,6 @@ class Images {
 
   static final String GIF_IMAGE = "gif-image.gif";
   static final String PNG_IMAGE = "png-image.png";
-  static final String JPEG_IMAGE = "jpeg-image.jpeg";
-
-  static final String NO_IMAGE = "no-image.txt";
 
   private Images() { }
 }
