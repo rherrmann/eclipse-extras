a430cb4bcf7238d8afaafabb756fc7c0e620018d
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2016-07-22 16:38:25 +0200

    Add private default constructor to avoid accidential instantiation

--- com.codeaffine.extras.imageviewer/src/com/codeaffine/extras/imageviewer/internal/ImageViewerPlugin.java
+++ com.codeaffine.extras.imageviewer/src/com/codeaffine/extras/imageviewer/internal/ImageViewerPlugin.java
@@ -5,4 +5,7 @@ public class ImageViewerPlugin {
 
   public static final String ID = "com.codeaffine.extras.imageviewer";
   public static final String IMAGE_CONTENT_TYPE_ID = "com.codeaffine.extras.imageviewer.ImageContentType";
+
+
+  private ImageViewerPlugin() { }
 }
