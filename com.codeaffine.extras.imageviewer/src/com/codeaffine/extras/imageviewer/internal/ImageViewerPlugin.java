2f43096a7b0851e26572d16314d7185e1f317f8a
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2016-08-04 17:21:33 +0200

    Provide icon for image viewer editor

--- com.codeaffine.extras.imageviewer/src/com/codeaffine/extras/imageviewer/internal/ImageViewerPlugin.java
+++ com.codeaffine.extras.imageviewer/src/com/codeaffine/extras/imageviewer/internal/ImageViewerPlugin.java
@@ -6,6 +6,5 @@ public class ImageViewerPlugin {
   public static final String ID = "com.codeaffine.extras.imageviewer";
   public static final String IMAGE_CONTENT_TYPE_ID = "com.codeaffine.extras.imageviewer.ImageContentType";
 
-
   private ImageViewerPlugin() { }
 }
