23eaa1af9e9de14c9d38f88fff0017e9cc489f99
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2016-10-22 14:40:56 +0200

    Extract method

--- com.codeaffine.extras.launch/src/com/codeaffine/extras/launch/internal/dialog/OpenLaunchDialogHander.java
+++ com.codeaffine.extras.launch/src/com/codeaffine/extras/launch/internal/dialog/OpenLaunchDialogHander.java
@@ -4,6 +4,8 @@ import static org.eclipse.jface.window.Window.OK;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchMode;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -16,9 +18,15 @@ public class OpenLaunchDialogHander extends AbstractHandler {
   public Object execute( ExecutionEvent event ) {
     LaunchSelectionDialog dialog = createDialog( event );
     if( dialog.open() == OK ) {
-      new LaunchConfigStarter( dialog.getLaunchMode(), dialog.getSelectedLaunchConfigurations() ).start();
+      startLaunchConfigs( dialog.getLaunchMode(), dialog.getSelectedLaunchConfigurations() );
     }
     return null;
+  }
+
+  private static void startLaunchConfigs( ILaunchMode launchMode,
+                                          ILaunchConfiguration[] launchConfigs )
+  {
+    new LaunchConfigStarter( launchMode, launchConfigs ).start();
   }
 
   protected LaunchSelectionDialog createDialog( ExecutionEvent event ) {
